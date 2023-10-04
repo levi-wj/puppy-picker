faa0bc1f534891429efca53f3620ed82d19f9a42
Levi Jones <leviwill.j@gmail.com>
2023-10-03 22:46:12 -0600

    Dog breeds get pulled from API. styled main page with dog cards

---
+++ src/lib/BreedCard.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+    export let breed: string;
+
+    const endpoint = `https://dog.ceo/api/breed/${breed}/images/random`;
+    const rotation = (Math.random() - .5) * 7;
+
+    async function getRandomImage(): Promise<string> {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return data.message;
+    }
+</script>
+
+<div style="transform: rotate({rotation}deg)">
+    {#await getRandomImage()}
+        <img src="#" alt="Loading..." height="300">
+    {:then src} 
+        <img src="{src}" class="doggy" alt="Loading..." height="300" loading="lazy">
+    {/await}
+    <img class="pawprint" src="Dog_Paw_Print.png" />
+    <h3>{breed}</h3>
+</div>
+
+<style>
+    div {
+        background-color: white;
+        border-radius: 4px;
+        padding: 1rem;
+        border: 1px solid rgb(229, 229, 229);
+        padding-top: 1em;
+        cursor: pointer;
+        box-shadow: 1px 3px 4px rgb(160, 160, 160);
+        transition: all .2s ease-in-out;
+        position: relative;
+    }
+    div:hover {
+        box-shadow: 1px 3px 6px gray;
+        border: 4px solid rgb(250, 197, 98);
+    }
+
+    h3 {
+        font-family: 'Caveat', cursive;
+        font-size: 25pt;
+        color: rgb(71, 71, 71);
+    }
+
+    img.doggy {
+        object-fit: contain;
+        width: 100%;
+        height: fit-content;
+        margin-bottom: 2rem;
+        max-height: 200px;
+    }
+    img.pawprint {
+        position: absolute;
+        height: 100px;
+        width: 100px;
+        opacity: .1;
+        right: .2rem;
+        bottom: 1rem;
+    }
+</style>
---
+++ src/lib/BreedList.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import BreedCard from "./BreedCard.svelte";
+
+    const endpoint: string = 'https://dog.ceo/api/breeds/list/all';
+    let shownBreeds: number = 10;
+
+    async function getBreeds(): Promise<string[]> {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        return data.message;
+    }
+</script>
+
+<div class="breed-list">
+    {#await getBreeds()}
+        <p>Fetching dogs...</p>
+    {:then breeds} 
+        {#each Object.keys(breeds) as breed, i}
+            {#if i < shownBreeds}
+                <BreedCard breed={breed} />
+            {/if}
+        {/each}
+    {/await}
+</div>
+
+<style>
+    .breed-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        gap: 3rem;
+        padding: 1rem;
+        border-radius: 100px;
+        background-color: rgb(255, 230, 230);
+    }
+</style>
